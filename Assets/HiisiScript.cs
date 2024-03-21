7814cf421d3593f90a3117a7d7d4760ec177b4e9
JusoPie <128028155+JusoPie@users.noreply.github.com>
2024-03-21 13:46:56 +0200

    Hiisi FIx

--- Assets/HiisiScript.cs
+++ Assets/HiisiScript.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class HiisiScript : MonoBehaviour
@@ -7,11 +8,14 @@ public class HiisiScript : MonoBehaviour
     public Transform target;
     public float enemySpeed = 1f;
     public float attackRange = 2f;
+    private Animator animator;
+    private bool isAttacking;
 
     // Start is called before the first frame update
     void Start()
     {
         target = GameObject.FindGameObjectWithTag("Player").transform;
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -22,11 +26,13 @@ public class HiisiScript : MonoBehaviour
         if (dist > attackRange)
         {
             enemySpeed = 1f;
+            isAttacking = false;
         }
 
         else 
         {
             enemySpeed = 0;
+            isAttacking = true;
         }
 
 
@@ -35,5 +41,31 @@ public class HiisiScript : MonoBehaviour
 
         transform.Rotate(90f, 0f, 0f, Space.Self);
         transform.Rotate(0f, 90f, 0f, Space.Self);
+
+        if (isAttacking)
+        {
+            Attack();
+        }
+
+        else 
+        {
+            StopAttacking();
+        }
+    }
+
+    
+
+    void Attack()
+    {
+        // Insert attack behavior here
+        
+
+        animator.SetBool("isAttacking", true);
+        animator.SetTrigger("attack");
+    }
+
+    void StopAttacking() 
+    {
+        animator.SetBool("isAttacking", false);
     }
 }
