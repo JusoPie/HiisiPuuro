5c2ad2c4a8c1376aa678c4d8befe8814af4236fd
JusoPie <128028155+JusoPie@users.noreply.github.com>
2024-03-27 19:34:32 +0200

    Player Health

--- Assets/Health.cs
+++ Assets/Health.cs
@@ -8,16 +8,26 @@ public class Health : MonoBehaviour
     public int maxHealth = 5;
     public int currentHealth;
     public GameObject gameOver;
+
+    public HealthBar healthBar;
+
     // Start is called before the first frame update
     void Start()
     {
+        ResetHealth();
+    }
+
+    public void ResetHealth() 
+    {
         currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
     }
 
     public void TakeDamage(int damageAmount)
     {
         currentHealth -= damageAmount;
-        
+        healthBar.SetHealth(currentHealth);
+
 
         if (currentHealth <= 0)
         {
