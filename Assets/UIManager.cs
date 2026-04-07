913ae2c0485599289bbc3cd3b82f1327b4a5fafd
JusoPie <128028155+JusoPie@users.noreply.github.com>
2026-04-07 21:14:45 +0300

    7.4.2026
    Arrow count text to TMPro TextMeshProUGUI

--- Assets/UIManager.cs
+++ Assets/UIManager.cs
@@ -2,11 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class UIManager : MonoBehaviour
 {
     [SerializeField]
-    private Text ammoText;
+    private TextMeshProUGUI ammoText;
 
     // Start is called before the first frame update
     void Start()
